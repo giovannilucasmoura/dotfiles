d13989764f4edc659c878c88f71708b0d949f451
Giovanni Lucas <giovannilucasmoura@gmail.com>
2023-08-06 22:21:31 -0300

    nvim: vim options

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,18 @@
-vim.opt.termguicolors = true -- use true term colors
+local opt = vim.opt
+local o = vim.o
+
+-- show absolute line numbers
+opt.number = true
+opt.relativenumber = false
+opt.termguicolors = true -- use true term colors
+opt.ignorecase = true -- ignore capitalization on searches
+opt.smartcase = true -- ...unless there's an uppercase letter
+opt.hlsearch = false -- disable highlighting of previous search
+
+o.tabstop = 4 -- how many characters a tab occupies
+o.softtabstop = 4 -- same thing but for soft tabs
+o.shiftwidth = 4 -- how much to indent when using << and >>
+o.expandtab = true -- transform tab insertion into multiple spaces
 
 vim.g.mapleader = " " -- set leader key as <space>
 
