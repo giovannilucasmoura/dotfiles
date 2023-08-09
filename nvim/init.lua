36b5a4b84e5c332fb87ecc05fe429de511fdb89d
Giovanni Lucas <giovannilucasmoura@gmail.com>
2023-08-08 23:13:12 -0300

    nvim: adding autoindent option

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,6 +13,7 @@ o.tabstop = 4 -- how many characters a tab occupies
 o.softtabstop = 4 -- same thing but for soft tabs
 o.shiftwidth = 4 -- how much to indent when using << and >>
 o.expandtab = true -- transform tab insertion into multiple spaces
+o.autoindent = true
 
 vim.g.mapleader = " " -- set leader key as <space>
 
