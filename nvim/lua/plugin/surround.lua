3c59a423bd69302e5cd986ef8bcefe881601cef1
Giovanni Lucas <giovannilucasmoura@gmail.com>
2023-08-10 22:16:39 -0300

    nvim: adding surround plugin

---
+++ nvim/lua/plugin/surround.lua
@@ -0,0 +1,5 @@
+return {
+  'echasnovski/mini.surround',
+  version = '*',
+  opts = { mappings = { add = 'msa', delete = 'msd', find = '<leader>fs', find_left = '<leader>fS', replace = 'msr' } },
+}
