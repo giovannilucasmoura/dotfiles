84c8993936b3880cb8ac1240c0cd65b59cfe405c
Giovanni Lucas <giovannilucasmoura@gmail.com>
2023-08-27 12:53:35 -0300

    nvim: Merging telescope and other navigation plugins into single file

--- nvim/lua/plugin/navigation.lua
+++ nvim/lua/plugin/navigation.lua
@@ -156,4 +156,57 @@ return {
   {
     'nvim-telescope/telescope-media-files.nvim',
   },
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    opts = {
+      diagnostics = {
+        enable = true,
+      },
+    },
+    keys = {
+      {
+        '<leader>e',
+        function()
+          require('nvim-tree.api').tree.toggle({ path = vim.fn.getcwd() })
+        end,
+        desc = 'Toggle Nvim-Tree',
+      },
+      {
+        '<leader>E',
+        function()
+          require('nvim-tree.api').tree.focus()
+        end,
+        desc = 'Refresh Nvim-Tree',
+      },
+    },
+    init = function(plugin)
+      require('nvim-tree').setup(plugin.opts)
+    end,
+  },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = { label = { rainbow = { enabled = true } } },
+    keys = {
+      {
+        '<leader>s',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash',
+      },
+      {
+        '<leader>S',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').treesitter()
+        end,
+        desc = 'Flash Treesitter',
+      },
+    },
+  },
 }
