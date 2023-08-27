6c71b05c0ac380329ebf7063c00c1d0721044a83
Giovanni Lucas <giovannilucasmoura@gmail.com>
2023-08-26 22:13:25 -0300

    nvim: merging text editing plugins into a single file

--- nvim/lua/plugin/editing.lua
+++ nvim/lua/plugin/editing.lua
@@ -1,5 +1,39 @@
 return {
   {
+    'numToStr/Comment.nvim',
+    opts = function()
+      local binds = {
+        line = '<leader>mc',
+        block = '<leader>mC',
+      }
+
+      return {
+        toggler = binds,
+        opleader = binds,
+        mappings = { basic = true, extra = false },
+      }
+    end,
+  },
+  {
+    'windwp/nvim-autopairs',
+    opts = {
+      disable_filetype = { 'TelescopePrompt', 'NvimTree' },
+    },
+    config = function(_, opts)
+      require('nvim-autopairs').setup({ opts })
+
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
+      local Rule = require('nvim-autopairs.rule')
+      local npairs = require('nvim-autopairs')
+
+      npairs.add_rules({ Rule('<', '>', { 'cs', 'csx', 'java' }) })
+    end,
+  },
+  {
     'mhartington/formatter.nvim',
     lazy = false,
     opts = function()
@@ -51,6 +85,19 @@ return {
         end,
         mode = { 'n', 'v' },
         desc = 'Format (external)',
+      },
+    },
+  },
+  {
+    'echasnovski/mini.surround',
+    version = '*',
+    opts = {
+      mappings = {
+        add = '<leader>msa',
+        delete = '<leader>msd',
+        find = '<leader>fs',
+        find_left = '<leader>fS',
+        replace = '<leader>msr',
       },
     },
   },
--- nvim/lua/plugin/surround.lua
+++
@@ -1,5 +0,0 @@
-return {
-  'echasnovski/mini.surround',
-  version = '*',
-  opts = { mappings = { add = '<leader>msa', delete = '<leader>msd', find = '<leader>fs', find_left = '<leader>fS', replace = '<leader>msr' } },
-}
