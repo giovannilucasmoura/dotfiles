c992a600ccae6fa375ba75e2d88e26908abacb3e
Giovanni Moura <giovannilucasmoura@gmail.com>
2023-10-22 20:47:37 -0300

    nixos: more packages (againer)

--- nixos/.nix/dev-packages.nix
+++ nixos/.nix/dev-packages.nix
@@ -7,9 +7,10 @@ in
     # Bash
     pkgs.shellcheck pkgs.shfmt
     # C \ C++
-    pkgs.gcc
+    pkgs.gcc pkgs.clang-tools pkgs.clang pkgs.libedit
     # HTML / CSS
-    pkgs.html-tidy pkgs.nodePackages_latest.stylelint pkgs.nodePackages_latest.js-beautify
+    pkgs.html-tidy pkgs.nodePackages_latest.stylelint
+    pkgs.nodePackages_latest.js-beautify
     # Java
     pkgs.jdk
     # Javascript
@@ -26,8 +27,8 @@ in
     pkgs.rustup
 
     # Other packages
-    pkgs.gnumake pkgs.pandoc pkgs.clang-tools pkgs.clang unstable.prettierd
-    pkgs.libxml2 pkgs.glslang
+    pkgs.gnumake pkgs.pandoc unstable.prettierd pkgs.libxml2 pkgs.glslang
+    pkgs.gdb pkgs.valgrind
   ];
 
   # Docker
--- nixos/.nix/packages.nix
+++ nixos/.nix/packages.nix
@@ -5,13 +5,13 @@ let
 in
 {
   nixpkgs.overlays = [
-    (import (builtins.fetchTarball https://github.com/nix-community/emacs-overlay/archive/master.tar.gz))
+    (import (builtins.fetchTarball "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz"))
   ];
 
   environment.systemPackages = with pkgs; [
-    alacritty chromium cmus cmusfm dunst emacs-unstable feh firefox-esr fd flameshot fzf gimp i3lock-color isync krita
-    lxappearance mpv mu neofetch pamixer pavucontrol picom polybar qbittorrent ranger redshift ripgrep rofi starship
-    syncthing xclip xf86_input_wacom xss-lock zathura zoxide xorg.libXi
+    alacritty chromium cmus cmusfm discord dunst emacs-unstable feh firefox-esr fd flameshot fzf gimp i3lock-color isync
+    krita lxappearance mpv mu neofetch pamixer pavucontrol picom polybar qbittorrent ranger redshift ripgrep rofi
+    starship syncthing xclip xf86_input_wacom xss-lock zathura zoxide xorg.libXi
   ] ++ unstablePkgs;
 
   programs = {
