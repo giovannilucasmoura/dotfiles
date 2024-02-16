6c1e234b9f0dcc2d713a679712d8961dc71ddd1a
Giovanni Lucas <giovannilucasmoura@gmail.com>
2024-02-16 15:51:18 -0300

    nixos: upgrade nixos and emacs

--- nixos/.nix/packages.nix
+++ nixos/.nix/packages.nix
@@ -1,11 +1,9 @@
 { pkgs, config, ... }:
+
 let
   xone-ovr = config.boot.kernelPackages.callPackage ./xone.nix {};
-  unstable = import <nixos-unstable> {};
-  unstablePkgs = with unstable; [ neovim zellij ];
   nurPkgs = with pkgs.nur.repos.nltch; [ spotify-adblock ciscoPacketTracer8 ];
-in
-{
+in {
   nixpkgs.config.packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball
       "https://github.com/nix-community/NUR/archive/master.tar.gz") {
@@ -13,18 +11,13 @@ in
       };
   };
 
-  nixpkgs.overlays = [
-    (import (builtins.fetchTarball
-      "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz"))
-  ];
-
   environment.systemPackages = with pkgs; [
-    alacritty appimage-run chromium cmus discord dunst easyeffects emacs-unstable
-    feh firefox-esr fd flameshot fzf gnupg gimp goverlay i3lock-color libreoffice
-    lxappearance mangohud mpv neofetch pamixer patchelf pavucontrol picard picom
-    playerctl polybar pinentry qbittorrent ranger redshift ripgrep rofi syncthing
-    unzip xclip xss-lock zathura zip zoxide xorg.libXi
-  ] ++ unstablePkgs ++ nurPkgs;
+    alacritty appimage-run chromium cmus discord dunst easyeffects feh
+    firefox-esr fd flameshot fzf gnupg gimp goverlay i3lock-color jdk libreoffice
+    lxappearance mangohud mpv neofetch neovim nodejs_20 pamixer patchelf pavucontrol picard
+    picom playerctl polybar pinentry qbittorrent ranger redshift ripgrep rofi
+    syncthing unzip xclip xss-lock zathura zellij zip zoxide xorg.libXi
+  ] ++ nurPkgs;
 
   programs = {
     i3lock.package = pkgs.i3lock-color;
@@ -50,7 +43,7 @@ in
   services = {
     emacs = {
       enable = true;
-      package = pkgs.emacs-unstable;
+      package = pkgs.emacs29;
     };
     pcscd.enable = true;
   };
